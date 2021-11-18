0aa0f8aca67580b9d667e993241259f50079e7f4
ajaybshinde21 <ajayshinde1@protonmail.com>
2021-11-18 16:11:54 +0530

    Solve Parikshak-PFCC-Motu

---
+++ practice-for-coding-challenge/Motu/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//Write a complete program to find the length of maximum subsequence of '1'
+// in the given string as '0' & '1' 
+//
+//if there is no subsequence of '1' print '-1
+
+
+public class Main {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	String inputString = input.nextLine();
+	String[] inputStringArray = inputString.split("0");
+	int max = inputStringArray[0].length();
+	for(int i = 0 ; i < inputStringArray.length; i++) {
+		if(max < inputStringArray[i].length())
+			max = inputStringArray[i].length();
+	}
+	if(max > 0) {
+		System.out.println(max);
+	}else {
+		System.out.println(-1);
+	}
+	}
+	
+}
